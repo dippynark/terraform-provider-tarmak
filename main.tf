9f0a0c0b2b23e977fe27e09a8bc2098bd982b59b
Luke Addison <lukeaddison785@gmail.com>
2018-02-13 15:37:36 +0000

    Update example

--- main.tf
+++ main.tf
@@ -18,18 +18,10 @@ data "tarmak_bastion_instance" "bastion" {
   username = "root"
 }
 
-output "bastion_status" {
-  value = "${data.tarmak_bastion_instance.bastion.status}"
-}
-
 data "tarmak_vault_cluster" "vault" {
   instances = ["ip-10-99-32-12.us-west-1.compute.internal",
 		"ip-10-99-32-11.us-west-1.compute.internal",
 		"ip-10-99-32-10.us-west-1.compute.internal"]
-}
-
-output "vault_status" {
-  value = "${data.tarmak_vault_cluster.vault.status}"
 }
 
 data "tarmak_vault_instance_role" "etcd" {
@@ -47,14 +39,22 @@ data "tarmak_vault_instance_role" "worker" {
   role_name = "worker" 
 }
 
-output "vault_instance_role_etcd_status" {
-  value = "${data.tarmak_vault_instance_role.etcd.status}"
+output "bastion_instance_status" {
+  value = "${data.tarmak_bastion_instance.bastion.status}"
 }
 
-output "vault_instance_role_master_status" {
-  value = "${data.tarmak_vault_instance_role.master.status}"
+output "vault_cluster_status" {
+  value = "${data.tarmak_vault_cluster.vault.status}"
 }
 
-output "vault_instance_role_worker_status" {
-  value = "${data.tarmak_vault_instance_role.worker.status}"
+output "vault_instance_role_etcd_init_token" {
+  value = "${data.tarmak_vault_instance_role.etcd.init_token}"
+}
+
+output "vault_instance_role_master_init_token" {
+  value = "${data.tarmak_vault_instance_role.master.init_token}"
+}
+
+output "vault_instance_role_worker_init_token" {
+  value = "${data.tarmak_vault_instance_role.worker.init_token}"
 }
